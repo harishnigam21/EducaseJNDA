32a28cda52f3ccd7795f6855db2fec536971a85f
Harish Nigam <harishnigam21@gmail.com>
2026-06-05 16:12:14 +0530

    Add DB pool options and GitHub fetch service
    Expose DB port and tighten DB pool configuration in config/db.js (port from env, waitForConnections: true, connectionLimit: 10, ssl.rejectUnauthorized: false) to improve connection handling. Add services/githubTask.js with fetchGithubProfile using axios to retrieve GitHub user data from the public API.

---
+++ services/githubTask.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const fetchGithubProfile = async (username) => {
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response.data;
+};
